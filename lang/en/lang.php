01da2b1df285e2eb91dabb4def6156a61bf7c9ea
Syed Abidur Rahman <aabid048@gmail.com>
2018-08-08 15:16:22 +0600

    Language typo has been fixed.

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,7 +2,7 @@
 
 return [
     'mail' => [
-        'sendgrid' => 'Sendrid',
+        'sendgrid' => 'Sendgrid',
         'sendgrid_api_key' => 'Sendgrid API Key',
         'sendgrid_api_key_comment' => 'Enter your Sendgrid API key.'
     ]
